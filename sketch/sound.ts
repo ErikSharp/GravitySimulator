e4c68e815629429e0d711e2242213762b54f64e6
Erik Sharp <mr.erik.sharp@gmail.com>
2026-09-09 23:03:31 +0100

    Add original chiptune background music

--- sketch/sound.ts
+++ sketch/sound.ts
@@ -1,12 +1,15 @@
 export class SoundEffects {
     private context: AudioContext | undefined;
     private lastThrustTime = 0;
+    private musicStarted = false;
+    private musicTimer: number | undefined;
 
     unlock(): void {
         if (!this.context) {
             this.context = new AudioContext();
         }
         void this.context.resume();
+        this.startMusic();
     }
 
     fire(): void {
@@ -23,6 +26,63 @@ export class SoundEffects {
 
     explosion(): void {
         this.tone("sawtooth", 180, 28, 0.38, 0.11);
+    }
+
+    private startMusic(): void {
+        if (!this.context || this.musicStarted) {
+            return;
+        }
+
+        this.musicStarted = true;
+        this.scheduleMusicLoop();
+        this.musicTimer = window.setInterval(() => this.scheduleMusicLoop(), 8000);
+    }
+
+    private scheduleMusicLoop(): void {
+        if (!this.context) {
+            return;
+        }
+
+        const start = this.context.currentTime + 0.08;
+        const beat = 0.25;
+        const melody = [
+            659, 784, 880, 784, 659, 587, 659, 523,
+            587, 659, 784, 880, 784, 659, 587, 523,
+            523, 659, 784, 988, 880, 784, 659, 587,
+            659, 784, 880, 784, 659, 587, 523, 392,
+        ];
+        const bass = [131, 165, 196, 165, 147, 175, 220, 175];
+
+        melody.forEach((frequency, index) => {
+            this.note("square", frequency, start + index * beat, beat * 0.8, 0.028);
+        });
+        bass.forEach((frequency, index) => {
+            const time = start + index * beat * 4;
+            this.note("triangle", frequency, time, beat * 1.65, 0.045);
+            this.note("triangle", frequency * 1.5, time + beat * 2, beat * 1.35, 0.025);
+        });
+    }
+
+    private note(
+        type: OscillatorType,
+        frequency: number,
+        startTime: number,
+        duration: number,
+        volume: number
+    ): void {
+        if (!this.context) {
+            return;
+        }
+
+        const oscillator = this.context.createOscillator();
+        const gain = this.context.createGain();
+        oscillator.type = type;
+        oscillator.frequency.setValueAtTime(frequency, startTime);
+        gain.gain.setValueAtTime(volume, startTime);
+        gain.gain.setValueAtTime(0.001, startTime + duration);
+        oscillator.connect(gain).connect(this.context.destination);
+        oscillator.start(startTime);
+        oscillator.stop(startTime + duration);
     }
 
     private tone(
