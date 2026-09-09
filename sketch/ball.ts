a28341c79183884306a76b007a508432cdca97b9
Erik Sharp <mr.erik.sharp@gmail.com>
2026-09-09 22:41:58 +0100

    Implement interactive gravity simulation

--- sketch/ball.ts
+++ sketch/ball.ts
@@ -1,29 +1,52 @@
-import { drawable } from "./drawable.js";
+import { drawable } from "./drawable";
 import p5 from "p5";
 
 export class Ball implements drawable {
-    position: p5.Vector;
-    velocity: p5.Vector;
+    readonly position: p5.Vector;
+    private trail: p5.Vector[] = [];
 
-    constructor(private p: p5) {
-        this.position = p.createVector(p.windowWidth / 2, p.windowHeight / 2);
-        this.velocity = p.createVector(1, 1);
+    constructor(
+        private p: p5,
+        x: number,
+        y: number,
+        readonly velocity: p5.Vector,
+        readonly mass: number,
+        readonly radius: number,
+        private readonly color: [number, number, number],
+        readonly fixed = false
+    ) {
+        this.position = p.createVector(x, y);
+    }
+
+    update(attractor: Ball): void {
+        if (this.fixed) {
+            return;
+        }
+
+        const direction = p5.Vector.sub(attractor.position, this.position);
+        const distanceSquared = this.p.constrain(direction.magSq(), 400, 250000);
+        const acceleration = (0.2 * attractor.mass) / distanceSquared;
+
+        direction.setMag(acceleration);
+        this.velocity.add(direction);
+        this.position.add(this.velocity);
+
+        this.trail.push(this.position.copy());
+        if (this.trail.length > 90) {
+            this.trail.shift();
+        }
     }
 
     draw(): void {
-        this.p.ellipseMode(this.p.CENTER);
+        this.p.noFill();
+        this.trail.forEach((point, index) => {
+            const opacity = (index / this.trail.length) * 120;
+            this.p.stroke(this.color[0], this.color[1], this.color[2], opacity);
+            this.p.point(point.x, point.y);
+        });
 
-        // ballX += xVelocity;
-        this.position = this.position.add(this.velocity);
-        // if (ballX > windowWidth || ballX < 0) {
-        //     xVelocity *= -1;
-        // }
-
-        // ballY += yVelocity;
-        // if (ballY > windowHeight || ballY < 0) {
-        //     yVelocity *= -1;
-        // }
-
-        this.p.ellipse(this.position.x, this.position.y, 80, 80);
+        this.p.noStroke();
+        this.p.fill(this.color[0], this.color[1], this.color[2]);
+        this.p.circle(this.position.x, this.position.y, this.radius * 2);
     }
 }
--- sketch/sketch.ts
+++ sketch/sketch.ts
@@ -1,22 +1,68 @@
 import p5 from "p5";
-// import { Ball } from "./ball.js";
-// import { drawable } from "./drawable.js";
+import { Ball } from "./ball";
 
-function sketch(p: p5) {
-    p.setup = function() {
-        p.createCanvas(p.windowWidth, p.windowHeight);
-        // drawables.push(new Ball());
+const sketch = (p: p5) => {
+    const bodies: Ball[] = [];
+    let attractor: Ball;
+    let launchStart: p5.Vector | undefined;
 
-        // ballX = windowWidth / 2;
-        // ballY = windowHeight / 2;
+    const addOrbitingBody = (
+        radius: number,
+        speed: number,
+        mass: number,
+        size: number,
+        color: [number, number, number]
+    ) => {
+        const centerX = p.width / 2;
+        const centerY = p.height / 2;
+        bodies.push(new Ball(p, centerX + radius, centerY, p.createVector(0, -speed), mass, size, color));
     };
 
-    p.draw = function() {
-        p.background(10);
-        // drawable1s.forEach(d => d.draw());
+    p.setup = () => {
+        document.body.style.margin = "0";
+        p.createCanvas(p.windowWidth, p.windowHeight);
+        p.frameRate(60);
 
-        // ellipseMode(CENTER);
-    }.bind(p);
-}
+        attractor = new Ball(p, p.width / 2, p.height / 2, p.createVector(), 2000, 26, [255, 204, 77], true);
+        addOrbitingBody(105, 2, 10, 7, [87, 183, 255]);
+        addOrbitingBody(185, 1.48, 14, 9, [255, 112, 112]);
+        addOrbitingBody(270, 1.22, 7, 6, [161, 241, 157]);
+    };
 
-let foo = new p5(sketch);
+    p.mousePressed = () => {
+        launchStart = p.createVector(p.mouseX, p.mouseY);
+    };
+
+    p.mouseReleased = () => {
+        if (!launchStart) {
+            return;
+        }
+
+        const launchVelocity = p5.Vector.sub(launchStart, p.createVector(p.mouseX, p.mouseY));
+        launchVelocity.mult(0.04);
+        bodies.push(new Ball(p, launchStart.x, launchStart.y, launchVelocity, 8, 6, [255, 255, 255]));
+        launchStart = undefined;
+    };
+
+    p.draw = () => {
+        p.background(8, 12, 24);
+        bodies.forEach((body) => body.update(attractor));
+        attractor.draw();
+        bodies.forEach((body) => body.draw());
+
+        if (launchStart) {
+            p.stroke(255, 255, 255, 180);
+            p.line(launchStart.x, launchStart.y, p.mouseX, p.mouseY);
+        }
+
+        p.noStroke();
+        p.fill(235);
+        p.textSize(16);
+        p.text("Gravity Simulator", 20, 32);
+        p.textSize(13);
+        p.fill(180);
+        p.text("Drag from the canvas to launch a new body", 20, 54);
+    };
+};
+
+new p5(sketch);
